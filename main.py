94492059f9add25d744fb8945f39618e77cb59eb
Martin Xi <martinashes@gmail.com>
2023-07-08 00:35:44 +0800

    get content from smzdm for furture process

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
 
-def get_article_content(url):
+def get_article_content(url, i):
+    folder_name = "0" + str(i)
+    file_name = "1.txt"
+    os.makedirs(folder_name, exist_ok=True)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
     # 发送HTTP请求并获取页面内容
@@ -13,33 +17,56 @@ def get_article_content(url):
     soup = BeautifulSoup(html_content, 'html.parser')
     title = get_title(soup)
     content = get_content(soup)
+    get_picture(soup, folder_name)
+
+    # 创建文件夹
+
+
+    # 构建文件路径
+    file_path = os.path.join(folder_name, file_name)
+
+    # 写入文件
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(title + "\n\n" + content)
 
 
 def get_content(soup):
-    selector = "#articleId > p"
-    text = []
-    for p in soup.select(selector):
-        print(p)
+    # 遍历所有匹配的节点
+    text = ""
+    for p in soup.find_all('p', itemprop='description'):
         # 提取文本内容
-    #     text = text.append(p.get_text().strip())
-    # print(text)
-    return text
+        node_text = p.get_text().strip()
 
+        # 如果节点有文本内容，则保存到text变量中
+        if node_text:
+            text += node_text + "\n"
+
+    # 输出保存的文本内容
+    return(text)
+
+
+def get_picture(soup, folder_name):
+    for p in soup.find_all('p', itemprop='description'):
+        # 查找节点下的所有<img>标签
+        images = p.find_all('img')
+        for img in images:
+            src = img['src']
+            response = requests.get(src)
+            filename = os.path.basename(src)
+            filepath = os.path.join(folder_name, filename)
+            with open(filepath, 'wb') as file:
+                file.write(response.content)
 
 
 def get_title(soup):
-    selector = "#articleId > h1"
-    selected_elements = soup.select(selector)
-    title = selected_elements[0].text.strip()
+    title = soup.find('title')
+    title = title.get_text().strip()
     return title
 
 
-
-def get_article_list():
+def get_article_list(url, pages):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-    url = "https://post.smzdm.com/tab/ribai/p"  # 替换为你要抓取的网页的URL
     links = []
-    pages = 4   # 需要翻页的次数
 
     for j in range(1, pages):
         url = format_url(j, url)
@@ -79,8 +106,11 @@ def format_url(i, url):
 
 
 def main():
-    url = "https://post.smzdm.com/p/ao968vp6/"
-    get_article_content(url)
+    url = "https://post.smzdm.com/tab/ribai/p"
+    links = get_article_list(url, 3)
+
+    for i in range(len(links)):
+        get_article_content(links[i], i)
 
 
 main()
